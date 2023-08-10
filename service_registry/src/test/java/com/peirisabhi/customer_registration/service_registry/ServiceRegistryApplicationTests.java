8deaa2efaad0018c2b44a70c236fa6faa8fce3ab
peirisabhi <abhishekdananjaya1@yahoo.com>
2023-08-10 18:36:24 +0000

    service registry added

---
+++ service_registry/src/test/java/com/peirisabhi/customer_registration/service_registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peirisabhi.customer_registration.service_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
