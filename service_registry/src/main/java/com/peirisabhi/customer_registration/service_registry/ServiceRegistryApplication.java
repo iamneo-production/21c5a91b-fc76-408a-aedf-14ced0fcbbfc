1cece21d40e0667cf4f05dfb78cc79da787a2990
peirisabhi <abhishekdananjaya1@yahoo.com>
2023-08-10 18:51:07 +0000

    Eureka configured

--- service_registry/src/main/java/com/peirisabhi/customer_registration/service_registry/ServiceRegistryApplication.java
+++ service_registry/src/main/java/com/peirisabhi/customer_registration/service_registry/ServiceRegistryApplication.java
@@ -2,8 +2,10 @@ package com.peirisabhi.customer_registration.service_registry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ServiceRegistryApplication {
 
 	public static void main(String[] args) {
