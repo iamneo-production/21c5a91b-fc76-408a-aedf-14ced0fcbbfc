023f3ea952ffafa3b20b7ffc67b30cc721a64051
peirisabhi <abhishekdananjaya1@yahoo.com>
2023-08-12 13:15:35 +0000

    account creation service added

---
+++ account_creation_service/src/main/java/com/houari/account_creation_service/AccountCreationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.houari.account_creation_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountCreationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountCreationServiceApplication.class, args);
+	}
+
+}
---
+++ account_creation_service/src/test/java/com/houari/account_creation_service/AccountCreationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.houari.account_creation_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountCreationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
